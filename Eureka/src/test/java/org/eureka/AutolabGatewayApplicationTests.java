e00a5fe6f727b274d1853a45675e3c5af9f16283
nacim.issam <nacim.issam@gmail.com>
2018-04-29 16:29:59 +0100

    setup eureka server

---
+++ Eureka/src/test/java/org/eureka/AutolabGatewayApplicationTests.java
@@ -0,0 +1,17 @@
+package org.eureka;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class AutolabGatewayApplicationTests {
+
+	//@Ignore
+	public void contextLoads() {
+	}
+
+}
