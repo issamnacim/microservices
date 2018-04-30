fa3db91706e0abc7431e9468060e3bd95841a151
nacim.issam <nacim.issam@gmail.com>
2018-04-30 22:50:11 +0100

    setup microservice 1

---
+++ service1/src/main/java/org/service/ServiceController.java
@@ -0,0 +1,19 @@
+package org.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created by issam on 2/27/18.
+ */
+@RestController
+public class ServiceController {
+
+    @GetMapping("Welcome")
+    public String welcome(){
+        return "Welcome to Account services ";
+    }
+
+
+}
